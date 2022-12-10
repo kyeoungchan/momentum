60805e9929e66f77505a4699eb065101448192d7
kyeoungchan <wkc1002@naver.com>
2022-12-10 18:22:40 +0900

    Add files via upload
    Created To Do List inputted by users

---
+++ js/todo.js
@@ -0,0 +1,24 @@
+const toDoForm = document.querySelector("#todo-form");
+const todoInput = toDoForm.querySelector("input");
+// const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.querySelector("#todo-list");
+
+function paintToDo(newTodo) {
+    // 입력받은 ToDo들을 Painting해주는 함수다.
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    // <li> Tag 내에 <span> Tag를 넣어준다.
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    // submit event를 실행시켰을 때 자동 새로고침을 방지해준다.
+    const newTodo = todoInput.value;
+    todoInput.value = "";
+    paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
