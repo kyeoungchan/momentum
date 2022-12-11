de5a9862e0f5efd5342cd3b9370a7a23a1077976
kyeoungchan <wkc1002@naver.com>
2022-12-11 23:54:44 +0900

    Add files via upload
    made deleting buttons with ToDo lists

--- js/todo.js
+++ js/todo.js
@@ -7,8 +7,12 @@ function paintToDo(newTodo) {
     // 입력받은 ToDo들을 Painting해주는 함수다.
     const li = document.createElement("li");
     const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "X";
     li.appendChild(span);
     // <li> Tag 내에 <span> Tag를 넣어준다.
+    li.appendChild(button);
+    // <span> Tag 다음으로 <button> Tag를 붙여준다.
     span.innerText = newTodo;
     toDoList.appendChild(li);
 }
